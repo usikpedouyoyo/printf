4488e1c784104de123a06a9ae815c0d0f090e982
uyoyo <uyoyoblessing@gmail.com>
2024-02-02 14:03:10 -0800

    new

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 		{
 			func_to_print_characters(format[i]);
 		}
-		 if (format[i] == '%' && format[i + 1] == 'c')
+		else if (format[i] == '%' && format[i + 1] == 'c')
 		{	
 		func_to_print_characters(va_arg(my_list_of_arguments, int));
 		i++;
